baf76648f6d6e424685fd15d9bf58d174a561991
Eugene White <eugenewhite1994@gmail.com>
2020-08-18 22:28:42 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -2,3 +2,19 @@
 
 
 
+class Person
+  ...
+ 
+  def get_married(person)
+    self.partner = person
+    person.partner = self
+  end
+ 
+  # Add the following two lines:
+  class PartnerError < StandardError
+  end
+end
+ 
+beyonce = Person.new("Beyonce")
+beyonce.get_married("Jay-Z")
+puts beyonce.name
